bbccfc2fe161fd4223f77b1b6de1a42e9a9e9920
shubham <49586542+shubham-dangi94@users.noreply.github.com>
2020-06-16 16:15:00 +0530

    Update child.java

--- child.java
+++ child.java
@@ -3,6 +3,7 @@ class child{
 public static void main(String args[])
 {
  System.out.println("this is my child made inside testchild branch");
+ System.out.println("changes made in master in child class");
 }
 
 
